8a302d3dabd7c551a6cf2c8f32254ac84f7a7344
Simon <semenh@gmail.com>
2015-01-18 20:08:07 +0200

    Added documentation

--- SMScrollView/SMScrollView.h
+++ SMScrollView/SMScrollView.h
@@ -10,14 +10,24 @@
 
 @interface SMScrollView : UIScrollView
 
+// default NO, if YES, fits the content to scrollView's bounds by changing zoomScale
 @property (nonatomic, assign) BOOL fitOnSizeChange;
+// default YES, fits the content to bounds only if they are bigger than content, ignored if fitOnSizeChange == YES
 @property (nonatomic, assign) BOOL upscaleToFitOnSizeChange;
+// default NO, sticks content bounds to scrollView edges instead of keeping center point in center, ignored if fitOnSizeChange == YES
 @property (nonatomic, assign) BOOL stickToBounds;
+// default YES, centers scrollView content in the center of its bounds
 @property (nonatomic, assign) BOOL centerZoomingView;
+// The view that was added by addViewForZooming: method
 @property (nonatomic, strong, readonly) UIView *viewForZooming;
+// Double tap gesture recognizer that zooms in/out the content
 @property (nonatomic, strong, readonly) UITapGestureRecognizer *doubleTapGestureRecognizer;
 
+// Fits the content to scrollView's bounds by changing zoomScale.
+// If fitOnSizeChange == YES, then this method is called automatically when scrollView's bounds have changed.
 - (void)scaleToFit;
+// Convenient method to add a view for zooming. The added view is available as viewForZooming property.
+// Adding new view for zooming will remove the previously added one.
 - (void)addViewForZooming:(UIView *)view;
 
 @end
