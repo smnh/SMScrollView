1b383c9f48789aeb14511292d5b303f37dcac8c9
Simon <semenh@gmail.com>
2015-01-19 15:05:28 +0200

    Updated documentation

--- Pod/Classes/SMScrollView.h
+++ Pod/Classes/SMScrollView.h
@@ -8,26 +8,60 @@
 
 #import <UIKit/UIKit.h>
 
+/**
+ * SMScrollView extends the functionality of UIScrollView.
+ * See its property documentation for more info.
+ */
 @interface SMScrollView : UIScrollView
 
-// default NO, if YES, fits the content to scrollView's bounds by changing zoomScale
+/**
+ * If YES, scales the content to fit to scrollView bounds when they change.
+ * If NO, the content point displayed in the center of scrollView bounds is
+ * kept in the center after scrollView bounds have changed (for example after
+ * interface orientation have changed).
+ * Default NO
+ */
 @property (nonatomic, assign) BOOL fitOnSizeChange;
-// default YES, fits the content to bounds only if they are bigger than content, ignored if fitOnSizeChange == YES
+
+/**
+ * If YES, scales the content to fit to scrollView bounds, but only if they are bigger than content.
+ * Default YES. Ignored if fitOnSizeChange == YES
+ */
 @property (nonatomic, assign) BOOL upscaleToFitOnSizeChange;
-// default NO, sticks content bounds to scrollView edges instead of keeping center point in center, ignored if fitOnSizeChange == YES
+
+/**
+ * If YES, sticks content bounds to scrollView edges instead of keeping center point in center.
+ * Default NO. Ignored if fitOnSizeChange == YES
+ */
 @property (nonatomic, assign) BOOL stickToBounds;
-// default YES, centers scrollView content in the center of its bounds
+
+/**
+ * Default YES, centers scrollView content in the center of its bounds
+ */
 @property (nonatomic, assign) BOOL centerZoomingView;
-// The view that was added by addViewForZooming: method
+
+/**
+ * The view that was added by addViewForZooming: method
+ * @see addViewForZooming:
+ */
 @property (nonatomic, strong, readonly) UIView *viewForZooming;
-// Double tap gesture recognizer that zooms in/out the content
+
+/**
+ * Double tap gesture recognizer that zooms in/out the content
+ */
 @property (nonatomic, strong, readonly) UITapGestureRecognizer *doubleTapGestureRecognizer;
 
-// Fits the content to scrollView's bounds by changing zoomScale.
-// If fitOnSizeChange == YES, then this method is called automatically when scrollView's bounds have changed.
+/**
+ * Scales the content to fit to scrollView bounds.
+ * If fitOnSizeChange == YES, then this method is called automatically when scrollView's bounds change.
+ */
 - (void)scaleToFit;
-// Convenient method to add a view for zooming. The added view is available as viewForZooming property.
-// Adding new view for zooming will remove the previously added one.
+
+/**
+ * Convenient method to add a view for zooming. The added view is available as viewForZooming property.
+ * Adding new view for zooming will remove the previously added one.
+ * @see viewForZooming
+ */
 - (void)addViewForZooming:(UIView *)view;
 
 @end
